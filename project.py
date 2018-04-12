e95a22bcaa66b51a298dd6922b140097ebf79fd4
Chawin Aiemvaravutigul <nick41746@hotmail.com>
2018-04-13 00:33:39 +0700

    Create security constraints

--- project.py
+++ project.py
@@ -1,25 +1,128 @@
 from __future__ import print_function
 from ortools.linear_solver import pywraplp
 
+class SolverLP:
 
-def createLP():
-    nodes = 0
-    edges = 0
+    solver = pywraplp.Solver('Network', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+    constraints = dict()
+    R = dict() # Messages in nodes
+    k = dict() # Random bits k (on edges)
+    e = dict() # Random bits e (on edges)
+    E = dict() # Random bits in nodes
+
+    def __init__(self, nodes, num_edges, edges, source, destination, graph, reverse_graph, s_to_d):
+        self.nodes = nodes
+        self.num_edges = num_edges
+        self.edges = edges
+        self.source = source
+        self.destination = destination
+        self.graph = graph
+        self.reverse_graph = reverse_graph
+        self.s_to_d = s_to_d
+
+    def createVariables(self):
+        INF = self.solver.infinity()
+        # Variables in nodes
+        for i in range(self.nodes):
+            self.R[i] = self.solver.NumVar(0, INF, 'R_'+str(i))
+            self.E[i] = self.solver.NumVar(0, INF, 'E_'+str(i))
+        
+        # Variables on edges
+        for u, v, d, r in self.edges:
+            edge = (u, v)
+            nm = 'k_('+str(u)+','+str(v)+')'
+            self.k[edge] = self.solver.NumVar(0, INF, nm)
+            nm = 'e_('+str(u)+','+str(v)+')'
+            self.e[edge] = self.solver.NumVar(0, INF, nm)
+            nm = 'R_('+str(u)+','+str(v)+')'
+            self.R[edge] = self.solver.NumVar(0, INF, nm)
+
+    def createSecurityConstraint(self):
+        INF = self.solver.infinity()
+        for u, v, d, r in self.edges:
+            edge = (u, v)
+            # Constraint 1 Random bits for generate Key
+            mp = ('Key', edge)
+            self.constraints[mp] = self.solver.Constraint(0, INF)
+            self.constraints[mp].SetCoefficient(self.k[edge], 1-d*r)
+            self.constraints[mp].SetCoefficient(self.e[edge], (1-d)*r)
+            self.constraints[mp].SetCoefficient(self.R[edge], -(1-r))
+
+            # Constraint 2 Capacity
+            mp = ('Cap', edge)
+            self.constraints[mp] = self.solver.Constraint(-INF, 1-d)
+            self.constraints[mp].SetCoefficient(self.R[edge], 1)
+            self.constraints[mp].SetCoefficient(self.k[edge], 1/r)
+            self.constraints[mp].SetCoefficient(self.e[edge], 1)
+
+            # Constraint 3 u has random bits for sending
+            mp = ('LM', edge)
+            self.constraints[mp] = self.solver.Constraint(-INF, 0)
+            self.constraints[mp].SetCoefficient(self.k[edge], 1-d*r)
+            self.constraints[mp].SetCoefficient(self.E[u], -(1-d)*r)
+            self.constraints[mp].SetCoefficient(self.e[edge], (1-d)*r)
+
+            # Constraint 4 Sum e_e = Sum e_e from Si
+            mp = ('Sum_e_e', edge)
+            self.constraints[mp] = self.solver.Constraint(0, 0)
+            self.constraints[mp].SetCoefficient(self.e[edge], 1)
+            for w in self.source:
+                s_e = (w, 'to', edge)
+                nm = 'E_('+str(w)+' to ('+str(edge)+'))'
+                self.e[s_e] = self.solver.NumVar(0, INF, nm)
+                self.constraints[mp].SetCoefficient(self.e[s_e], -1)
+
+            # Constraint 5 Sum ke = Sum ke from Si
+            mp = ('Sum_k_e', edge)
+            self.constraints[mp] = self.solver.Constraint(0, 0)
+            self.constraints[mp].SetCoefficient(self.k[edge], 1)
+            for w in self.source:
+                s_e = (w, 'to', edge)
+                nm = 'k_('+str(w)+' to ('+str(edge)+'))'
+                self.k[s_e] = self.solver.NumVar(0, INF, nm)
+                self.constraints[mp].SetCoefficient(self.k[s_e], -1)
+
+        for u in range(self.nodes):
+            if u not in self.source:
+                # Constraint 6 Eu = Sum Eu from Si
+                mp = ('Sum_E_u', u)
+                self.constraints[mp] = self.solver.Constraint(0, 0)
+                self.constraints[mp].SetCoefficient(self.E[u], 1)
+                for s in self.source:
+                    s_u = (s, 'to', u)
+                    nm = 'E_('+str(s)+' to '+str(u)+')'
+                    self.E[s_u] = self.solver.NumVar(0, INF, nm)
+                    self.constraints[mp].SetCoefficient(self.E[s_u], -1)
+
+    def createFlowConstraint(self):
+        INF = self.solver.infinity()
+
+    def createConstraints(self):
+        self.createSecurityConstraint()
+        self.createFlowConstraint()
+    
+    def Solve(self):
+        self.createVariables()
+        self.createConstraints()
+        self.objective = self.solver.Objective()
+        print(self.solver.NumConstraints())
+
+
+def main():
     g = dict()
     rg = dict()
-    adj = dict()
     source = []
     destination = []
     source_len = 0
     des_len = 0
-    print("Number of nodes: ", end="")
     nodes = int(input())
-    print("Number of edges: ", end="")
     edges = int(input())
+    edge = []
+    s_to_d = []
     for i in range(edges):
-        print("#%d (u, v, d, r) = " % (i+1), end="")
         inp = input().split()
         u, v, d, r = int(inp[0]), int(inp[1]), float(inp[2]), float(inp[3])
+        edge.append((u, v, d, r))
         if u in g:
             g[u].append((v, d, r, i))
         else:
@@ -29,82 +132,16 @@ def createLP():
         else:
             rg[v] = [(u, d, r, i)]
 
-    print("Number of source: ", end="")
     source_len = int(input())
     for i in range(source_len):
         source.append(int(input()))
-    print("Number of destination: ", end="")
     des_len = int(input())
     for i in range(des_len):
         destination.append(int(input()))
-    
-    solver = pywraplp.Solver('Network', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
-
-    queue = []
-    K = [None] * edges
-    R = [None] * edges
-    print(R)
-    for i in range(edges):
-        K[i] = solver.NumVar(0, solver.infinity(), 'K'+str(i+1))
-        R[i] = solver.NumVar(0, solver.infinity(), 'R'+str(i+1))
-        
-    for i in range(source_len):
-        queue.append(source[i])
-
-    visited = [False] * nodes
-
-    constraints = dict()
-
-    while len(queue) != 0:
-        u = queue[0]
-        queue.pop(0)
-        if visited[u] == True:
-            continue
-        visited[u] = True
-        if u in destination:
-            continue
-        for v, d, r, i in g[u]:
-            print(v, d, r, i)
-            constraints[(u, v)] = dict()
-            constraints[(u, v)]['Key'] = solver.Constraint(0, solver.infinity())
-            constraints[(u, v)]['Key'].SetCoefficient(K[i], 1)
-            constraints[(u, v)]['Key'].SetCoefficient(R[i], -(1-r)/(1-d*r))
-            constraints[(u, v)]['Cap'] = solver.Constraint(-solver.infinity(), 1)
-            constraints[(u, v)]['Cap'].SetCoefficient(K[i], 1/((1-d)*r))
-            constraints[(u, v)]['Cap'].SetCoefficient(R[i], 1/(1-d))
-            if v not in destination:
-                for vv, dd, rr, ii in g[v]:
-                    if vv in constraints:
-                        constraints[vv].SetCoefficient(K[i], (-1/r)*((1-dd)*rr/(1-dd*rr)))
-                    else:
-                        constraints[vv] = solver.Constraint(-solver.infinity(), 0)
-                        constraints[vv].SetCoefficient(K[ii], 1)
-                        constraints[vv].SetCoefficient(K[i], (-1/r)*((1-dd)*rr/(1-dd*rr)))
-            queue.append(v)
-    
-    constraints['Data'] = solver.Constraint(-solver.infinity(), 0)
-    for u in source:
-        for v, d, r, i in g[u]:
-            constraints['Data'].SetCoefficient(R[i], -1)
-    for u in destination:
-        for v, d, r, i in rg[u]:
-            constraints['Data'].SetCoefficient(R[i], 1)
-
-    objective = solver.Objective()
-    for u in destination:
-        for v, d, r, i in rg[u]:
-            objective.SetCoefficient(R[i], 1)
-    objective.SetMaximization()
-
-    solver.Solve()
-    opt_solution = 0
-    for u in destination:
-        for v, d, r, i in rg[u]:
-            opt_solution += R[i].solution_value()
-    
-    print('Number of variables =', solver.NumVariables())
-    print('Number of constraints =', solver.NumConstraints())
-    print('Optimal objective value =', opt_solution)
+    for i in range(int(input())):
+        s_to_d.append((int(input()), int(input())))
+    solv = SolverLP(nodes, edges, edge, source, destination, g, rg, s_to_d)
+    solv.Solve()
 
 if __name__ == '__main__':
-    createLP()
+    main()
--- test.py
+++ test.py
@@ -12,11 +12,13 @@ def YNetwork(v1, v2):
   k1 = solver.NumVar(0, solver.infinity(), 'k1')
   k2 = solver.NumVar(0, solver.infinity(), 'k2')
   k3 = solver.NumVar(0, solver.infinity(), 'k3')
+  RP1 = solver.NumVar(0, solver.infinity(), 'RP1')
+  RP2 = solver.NumVar(0, solver.infinity(), 'RP2')
   d1, de1 = 0.2, 0.05
   d2, de2 = 0.3, 0.05
   d3, de3 = 0.25, 0.05
 
-  c1 = solver.Constraint(0, solver.infinity())
+  c1 = solver.Constraint(0, solver.infinity(), 'c1')
   c1.SetCoefficient(k1, 1)
   c1.SetCoefficient(R1, -(1-de1)/(1-d1*de1))
 
@@ -47,13 +49,24 @@ def YNetwork(v1, v2):
   c7.SetCoefficient(k2, -(1-d3)*de3/(de2*(1-d3*de3)))
   c7.SetCoefficient(k1, -(1-d3)*de3/(de1*(1-d3*de3)))
   
+  c8 = solver.Constraint(-solver.infinity(), 0)
+  c8.SetCoefficient(k1, 1)
+  c8.SetCoefficient(RP1, -(1-d1)*de1/(1-d1*de1))
+
+  c9 = solver.Constraint(-solver.infinity(), 0)
+  c9.SetCoefficient(k2, 1)
+  c9.SetCoefficient(RP2, -(1-d2)*de2/(1-d2*de2))
+
   objective = solver.Objective()
   objective.SetCoefficient(R1, v1)
   objective.SetCoefficient(R2, v2)
   objective.SetMaximization()
 
 
-  print(solver.RowConstraint().GetCoefficient(k1))
+  # print(solver.RowConstraint().GetCoefficient(k1))
+  # print(solver.LookupConstraint('c1').GetCoefficient(k1))
+  print(dir(c9.this))
+
   # Solve the system.
   solver.Solve()
   opt_solution = R1.solution_value() + R2.solution_value()
